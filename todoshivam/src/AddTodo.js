3470e47986f481fe6b4bc8ad2e2e65e644174ac4
Shivam Sikdar <sikdarshivam@gmail.com>
2022-04-25 22:37:12 +0530

    todos and work related with that

---
+++ todoshivam/src/AddTodo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class AddTodo extends Component {
+    state= {
+        title: ''
+    }
+    handleChange =(e)=>{
+        this.setState({
+            title: e.target.value
+        })
+    }
+    handleSubmit =(e)=>{
+        e.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({
+            title: ''
+        })
+    }
+
+render ()   {
+  return (
+    <div>
+        <form onSubmit= {this.handleSubmit} >
+            <label>Add New Todo's</label>
+            <input type='text' onChange={this.handleChange} value={this.state.title}/>
+        </form>
+    </div>
+  )
+}
+}
+
+export default AddTodo
